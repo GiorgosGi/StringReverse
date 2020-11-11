a83aa0e762421e890157f1677319653788a9e5ac
Giorgos Gial <g.gialouris@outlook.com.gr>
2020-11-11 13:20:50 +0200

    Add project files.

---
+++ StringReverse/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Giorgos";
+            Console.WriteLine(StringReverse(name));
+            Console.WriteLine(StringReverse2(name));
+        }
+
+        static string StringReverse(string s)
+        {​​
+            string result = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {​​
+                result += s[i];
+            }​​
+            return (result);
+        }​​
+
+        static string StringReverse2(string s)
+        {​​
+                StringBuilder result = new StringBuilder();
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {​​
+                    result.Append(s[i]);
+            }​​
+                return (result.ToString());
+        }​​
+
+    }
+}
